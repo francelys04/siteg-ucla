40f144c7bf49e70454f23234f2a3734824a174fb
h.francelys <h.francelys@gmail.com>
2013-12-15 21:43:22 +0000

    Cambios en el controlador estudiante y VEstudiante, creaccion de CCatalago estudiante

---
+++ SITEG/src/controlador/CCatalogoEstudiante.java
@@ -0,0 +1,172 @@
+package controlador;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import modelo.Estudiante;
+import modelo.Programa;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Radio;
+import org.zkoss.zul.Radiogroup;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+import configuracion.GeneradorBeans;
+
+import servicio.SEstudiante;
+import servicio.SPrograma;
+
+public class CCatalogoEstudiante extends CGeneral{
+	
+	SEstudiante servicioEstudiante = GeneradorBeans.getServicioEstudiante();
+	SPrograma servicioPrograma = GeneradorBeans.getServicioPrograma();
+	@Wire
+	private Combobox cmbProgramaEstudiante;
+	@Wire
+	private Textbox txtCedulaEstudiante;
+	@Wire
+	private Textbox txtNombreEstudiante;
+	@Wire
+	private Textbox txtApellidoEstudiante;
+	@Wire
+	private Radiogroup rdgSexoEstudiante;
+	@Wire
+	private Radio rdoSexoFEstudiante;
+	@Wire
+	private Radio rdoSexoMEstudiante;
+	@Wire
+	private Textbox txtDireccionEstudiante;
+	@Wire
+	private Textbox txtTelefonoMovilEstudiante;
+	@Wire
+	private Textbox txtTelefonoFijoEstudiante;
+	@Wire
+	private Textbox txtCorreoEstudiante;
+	
+	@Wire
+	private Listbox ltbEstudiante;
+	@Wire
+	private Window wdwCatalogoEstudiante;
+	@Wire
+	private Textbox txtCedulaMostrarEstudiante;
+	@Wire
+	private Textbox txtNombreMostrarEstudiante;
+	@Wire
+	private Textbox txtApellidoMostrarEstudiante;
+	@Wire
+	private Textbox txtCorreoMostrarEstudiante;
+	@Wire
+	private Textbox txtProgramaMostrarEstudiante;
+	private  static String vistaRecibida;
+
+	
+	@Override
+	void inicializar(Component comp) {
+		// TODO Auto-generated method stub
+		List<Programa> programas = servicioPrograma.buscarActivas();
+		List<Estudiante> estudiantes = servicioEstudiante.buscarActivos();
+
+		if (cmbProgramaEstudiante == null) {
+			ltbEstudiante.setModel(new ListModelList<Estudiante>(estudiantes));
+		} else {
+			cmbProgramaEstudiante.setModel(new ListModelList<Programa>(
+					programas));
+		}
+
+		Selectors.wireComponents(comp, this, false);
+
+		HashMap<String, Object> map = (HashMap<String, Object>) Sessions
+				.getCurrent().getAttribute("itemsCatalogo");
+		
+	}
+	
+	// Metodo que permite el filtrado en el catalogo, por medio de los
+		// diferentes campos que este posee
+		@Listen("onChange = #txtCedulaMostrarEstudiante,#txtNombreMostrarEstudiante,#txtApellidoMostrarEstudiante,#txtCorreoMostrarEstudiante,#txtProgramaMostrarEstudiante")
+		public void filtrarDatosCatalogo() {
+			List<Estudiante> estudiantes1 = servicioEstudiante.buscarActivos();
+			List<Estudiante> estudiantes2 = new ArrayList<Estudiante>();
+
+			for (Estudiante estudiante : estudiantes1) {
+				if (estudiante
+						.getCedula()
+						.toLowerCase()
+						.contains(
+								txtCedulaMostrarEstudiante.getValue().toLowerCase())
+						&& estudiante
+								.getNombre()
+								.toLowerCase()
+								.contains(
+										txtNombreMostrarEstudiante.getValue()
+												.toLowerCase())
+						&& estudiante
+								.getApellido()
+								.toLowerCase()
+								.contains(
+										txtApellidoMostrarEstudiante.getValue()
+												.toLowerCase())
+						&& estudiante
+								.getCorreoElectronico()
+								.toLowerCase()
+								.contains(
+										txtCorreoMostrarEstudiante.getValue()
+												.toLowerCase())
+						&& estudiante
+								.getPrograma()
+								.getNombre()
+								.toLowerCase()
+								.contains(
+										txtProgramaMostrarEstudiante.getValue()
+												.toLowerCase())) {
+					estudiantes2.add(estudiante);
+				}
+
+			}
+
+			ltbEstudiante.setModel(new ListModelList<Estudiante>(estudiantes2));
+
+		}
+		public void recibir (String vista)
+		{
+			vistaRecibida = vista;
+			
+		}
+		
+		// Metodo que luego de presionar doble click sobre una fila del catalogo
+		// almacena los datos en un mapa, para luego colocarlos en la vista
+		// correspondiente
+		@Listen("onDoubleClick = #ltbEstudiante")
+		public void mostrarDatosCatalogo() { 			
+			Listitem listItem = ltbEstudiante.getSelectedItem();
+			Estudiante estudianteDatosCatalogo = (Estudiante) listItem.getValue();
+			final HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("cedula", estudianteDatosCatalogo.getCedula());
+			String vista = vistaRecibida;
+			map.put("vista", vista);
+			Sessions.getCurrent().setAttribute("itemsCatalogo", map);
+			Executions.sendRedirect("/vistas/arbol.zul");			
+			wdwCatalogoEstudiante.onClose();
+			
+
+		}
+
+		
+		/**
+		 * Metodo para recibir la vista a la que va dirigida el catalogo 
+		 * 
+		 * @date 15-12-2013
+		 */
+		
+}
