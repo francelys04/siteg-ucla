6a231ae8286323ae073006493215efba6658abf5
kairinj <kairinj@gmail.com>
2013-12-16 01:03:14 +0000

    modificaciones en el controlador y vista de programa

--- SITEG/src/controlador/CPrograma.java
+++ SITEG/src/controlador/CPrograma.java
@@ -24,10 +24,12 @@ import org.zkoss.zk.ui.Components;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Path;
 import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Grid;
@@ -53,7 +55,8 @@ public class CPrograma extends CGeneral {
 	 * operaciones con las entidades relacionadas con base de datos
 	 */
 	SPrograma servicioPrograma = GeneradorBeans.getServicioPrograma();
-	SAreaInvestigacion servicioArea = GeneradorBeans.getServicioArea();
+	
+	CCatalogoPrograma catalogo = new CCatalogoPrograma();
 	/*
 	 * Declaracion de componentes dado a sus id, implementados en las vistas
 	 * VPrograma y VCatalago
@@ -70,6 +73,11 @@ public class CPrograma extends CGeneral {
 	private Textbox txtNombreMostrarPrograma;
 	@Wire
 	private Textbox txtDescripcionMostrarPrograma;
+	@Wire
+	private Button btnEliminarPrograma;
+	
+	@Wire
+	private Button btnGuardarPrograma;
 
 	/*
 	 * Inicializacion de la varible global id que sera asociado con el id del
@@ -88,7 +96,7 @@ public class CPrograma extends CGeneral {
 		 * estatus=true con el servicioPrograma mediante el metodo buscarActivas
 		 */
 		List<Programa> programas = servicioPrograma.buscarActivas();
-		List<AreaInvestigacion> areas = servicioArea.buscarActivos();
+		
 		/*
 		 * Validacion para mostrar el listado de programas mediante el
 		 * componente ltbPrograma dependiendo si se encuentra ejecutando la
@@ -121,6 +129,7 @@ public class CPrograma extends CGeneral {
 				Programa programa = servicioPrograma.buscar(id);
 				txtNombrePrograma.setValue(programa.getNombre());
 				txtDescripcionPrograma.setValue(programa.getDescripcion());
+				btnEliminarPrograma.setDisabled(false);
 				map.clear();
 				map = null;
 			}
@@ -135,127 +144,74 @@ public class CPrograma extends CGeneral {
 		Window window = (Window) Executions.createComponents(
 				"/vistas/catalogos/VCatalogoPrograma.zul", null, null);
 		window.doModal();
+		catalogo.recibir("maestros/VPrograma");
 
 	}
+
+	
+
+	
 
 	// Metodo para guardar un programa
-	@Listen("onClick = #btnGuardarPrograma")
-	public void guardarPrograma() {
+		@Listen("onClick = #btnGuardarPrograma")
+		public void guardarPrograma() {
+			if (txtNombrePrograma.getValue() == ""
+					|| txtDescripcionPrograma.getValue() == "") {
+				Messagebox.show("Debe completar todos los campos", "Error",
+						Messagebox.OK, Messagebox.ERROR);
 
-		String nombre = txtNombrePrograma.getValue();
-		String descripcion = txtDescripcionPrograma.getValue();
-		Boolean estatus = true;
-		Programa programa = new Programa(id, nombre, descripcion, estatus);
-		/*
-		 * Permite guardar el objeto programa con el servicioPrograma mediante
-		 * el metodo guardar dado el programa anteriormente cargado;
-		 */
-		servicioPrograma.guardar(programa);
-		cancelarPrograma();
-		System.out.println("Programa Guardado");
-	}
+			} else {
+				Messagebox.show("Desea guardar el programa?",
+						"Dialogo de confirmacion", Messagebox.OK
+								| Messagebox.CANCEL, Messagebox.QUESTION,
+						new org.zkoss.zk.ui.event.EventListener() {
+							public void onEvent(Event evt)
+									throws InterruptedException {
+								if (evt.getName().equals("onOK")) {
+									String nombre = txtNombrePrograma.getValue(); 
+									String descripcion = txtDescripcionPrograma.getValue(); 
+									Boolean estatus = true; 
+									Programa programa = new Programa( id,nombre, descripcion, estatus);
+									servicioPrograma.guardar(programa);
+									cancelarPrograma(); 
+									Messagebox.show("Programa registrado exitosamente","Informacion", Messagebox.OK,Messagebox.INFORMATION); 
+									id = 0;
+									}
+							}
+						});
 
-	// Metodo para eliminar un programa dado su id
-	@Listen("onClick = #btnEliminarPrograma")
-	public void eliminarPrograma() {
-		/*
-		 * Creacion de objeto cargado con el servicioPrograma mediante el metodo
-		 * buscar dado a su id
-		 */
-		Programa programa = servicioPrograma.buscar(id);
-		programa.setEstatus(false);
-		/*
-		 * Permite eliminar logicamente (Cambio de estatus) el programa con el
-		 * servicioPrograma mediante el metodo guardad dado el programa
-		 * anteriormente cargado
-		 */
-		servicioPrograma.guardar(programa);
-		cancelarPrograma();
-		System.out.println("Programa Eliminado");
-	}
-
-	// Metodo para limpiar los componentes textbox y variable id
-	@Listen("onClick = #btnCancelarPrograma")
-	public void cancelarPrograma() {
-		id = 0;
-		txtNombrePrograma.setValue("");
-		txtDescripcionPrograma.setValue("");
-
-	}
-
-	/*
-	 * Metodo para filtrar los programas dado a el nombre y descripcion y del
-	 * programa en la vista VCatalogoPrograma
-	 */
-	@Listen("onChange = #txtNombreMostrarPrograma,#txtDescripcionMostrarPrograma")
-	public void filtrarDatosCatalogo() {
-		List<Programa> programas1 = servicioPrograma.buscarActivas();
-		List<Programa> programas2 = new ArrayList<Programa>();
-		/*
-		 * Ciclo que permite recorrer cada uno de los programas asociandolo con
-		 * el filtreo del nombre y descripcion del programa
-		 */
-		for (Programa programa : programas1) {
-			if (programa
-					.getNombre()
-					.toLowerCase()
-					.contains(txtNombreMostrarPrograma.getValue().toLowerCase())
-					&& programa
-							.getDescripcion()
-							.toLowerCase()
-							.contains(
-									txtDescripcionMostrarPrograma.getValue()
-											.toLowerCase())) {
-				programas2.add(programa);
 			}
+		}
+		
+		// Metodo para eliminar un programa dado su id
+		@Listen("onClick = #btnEliminarPrograma")
+		public void eliminarPrograma() {
+			Messagebox.show("Desea eliminar el programa?",
+					"Dialogo de confirmaci�n", Messagebox.OK | Messagebox.CANCEL,
+					Messagebox.QUESTION, new org.zkoss.zk.ui.event.EventListener() {
+						public void onEvent(Event evt) throws InterruptedException {
+							if (evt.getName().equals("onOK")) {
+								Programa programa = servicioPrograma.buscar(id);
+								programa.setEstatus(false);
+								servicioPrograma.guardar(programa);
+								cancelarPrograma();
+								Messagebox.show("Programa eliminado exitosamente", "Informacion", Messagebox.OK,Messagebox.INFORMATION);
+								btnEliminarPrograma.setDisabled(true);
+							}
+						}
+					});
+		}
+			// Metodo para limpiar los componentes textbox y variable id
+		@Listen("onClick = #btnCancelarPrograma")
+		public void cancelarPrograma() {
+			id = 0;
+			txtNombrePrograma.setValue("");
+			txtDescripcionPrograma.setValue("");
+			btnEliminarPrograma.setDisabled(false);
 
 		}
 
-		ltbPrograma.setModel(new ListModelList<Programa>(programas2));
+		
 
-	}
 
-	/*
-	 * Metodo que permite pasar los datos del programa a VPrograma mostrado en
-	 * la vista VCatalogoPrograma
-	 */
-	@Listen("onDoubleClick = #ltbPrograma")
-	public void mostrarDatosCatalogo() {
-
-		// Programa seleccionado en el catalogo VCatalogoPrograma
-		Listitem listItem = ltbPrograma.getSelectedItem();
-
-		/*
-		 * Creacion de objecto cargado con el servicioPrograma la cual mediante
-		 * el metodo buscarPorNombrePrograma nos retornara los datos del
-		 * programa dado al valor del item seleccionado en la vista
-		 * VCatalgoPrograma
-		 */
-		Programa programaDatos = servicioPrograma
-				.buscarPorNombrePrograma(((Programa) listItem.getValue())
-						.getNombre());
-		/*
-		 * Permite asignar uno o mas valores para poder ser utilizado en otra
-		 * region del proyecto
-		 */
-		final HashMap<String, Object> map = new HashMap<String, Object>();
-		/*
-		 * Permite guardar el id del programa seleccionado asociandolo con el
-		 * nombre de id en la variable map
-		 */
-		map.put("id", programaDatos.getId());
-		/*
-		 * Permite asignar el map asociandolo con el nombre de programasCatalogo
-		 * utilizando Sessions, setiandolo para posteriormente retornar su valor
-		 * y asi transportar los datos a la vista VPrograma
-		 */
-		String vista = "maestros/VPrograma";
-		map.put("vista", vista);
-		Sessions.getCurrent().setAttribute("itemsCatalogo", map);
-		// Permite abrir la vista VPrograma
-		Executions.sendRedirect("/vistas/arbol.zul");
-		// Permite cerrar la vista VCatalogo
-		wdwCatalogoPrograma.onClose();
-
-	}
 }
