b5607fe5e73ffc01cf1acf73e012c1dd5824dfb1
mariennis <mariennis@gmail.com>
2013-12-24 00:07:06 +0000

    creacion de las clases  CEvaluarAvances, IAvanceDAO, SAvance y modificaciones en VEvaluarAvances, CatalogoEvaluarAvances y GeneradorBeans

---
+++ SITEG/src/interfazdao/IAvanceDAO.java
@@ -0,0 +1,22 @@
+package interfazdao;
+
+
+import java.util.List;
+
+import modelo.Actividad;
+import modelo.Avance;
+import modelo.Lapso;
+import modelo.Programa;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface IAvanceDAO extends JpaRepository<Actividad, Long> {
+
+
+	public List<Avance> findByEstatusTrue();
+
+	public Avance findById(long id);
+}
+
---
+++ SITEG/src/servicio/SAvance.java
@@ -0,0 +1,30 @@
+package servicio;
+
+import java.util.List;
+import modelo.Avance;
+
+import interfazdao.IAvanceDAO;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class SAvance {
+	
+	@Autowired
+	private IAvanceDAO interfazAvance;
+
+	public Avance buscarAvancePorId(long id) {
+		// TODO Auto-generated method stub
+		Avance avance;
+		avance = interfazAvance.findById(id);
+		return avance;
+	}
+	
+	public List<Avance> buscarActivos() {
+		// TODO Auto-generated method stub
+		List<Avance> avance;
+		avance = interfazAvance.findByEstatusTrue();
+		return avance;
+	}
+}
