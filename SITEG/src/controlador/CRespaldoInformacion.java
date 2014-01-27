4ca7bea5ba081449e072fbd672f19f744fca67d6
adrianadelvalle90 <adrianadelvalle90@gmail.com>
2014-01-27 20:37:21 +0000

    se cambio ubicacion del metodo utilidad para respaldar la informacion

--- SITEG/src/controlador/CRespaldoInformacion.java
+++ SITEG/src/controlador/CRespaldoInformacion.java
@@ -12,8 +12,6 @@ import org.zkoss.zul.Button;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Window;
 
-import configuracion.UtilidadSiteg;
-
 @Controller
 public class CRespaldoInformacion extends CGeneral {
 
@@ -49,7 +47,8 @@ public class CRespaldoInformacion extends CGeneral {
 	    	   String fecha= new Date().toLocaleString();
 	    	   String fecha1= "("+fecha.substring(0, 2)+"-"+fecha.substring(3, 5)+"-"+fecha.substring(6, 10)+")";
 	    	   String nombre = "SITEG"+fecha1+".sql";
-	    	   String ruta = UtilidadSiteg.obtenerDirectorio()+"SITEG/respaldos/";
+	    	   String ruta = obtenerDirectorio()+"SITEG/respaldos/";
+	    	   System.out.println("ruta"+ruta);
 	    	   Runtime.getRuntime ().exec ("pg_dump -i -h localhost -p 5432 -U postgres -F c -v -f "+ruta+nombre+" siteg");
 	    	   Executions.getCurrent().sendRedirect("/respaldos/"+nombre, "_blank");
 	       } catch (IOException e) {
