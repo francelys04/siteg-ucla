b91a88d512d50c66228b1c4fa06cd73f578ca463
davidrodriguez100415 <davidrodriguez100415@gmail.com>
2014-01-28 03:13:28 +0000

    Modificacion de reporte de Tematicas mas Solicitadas y creacion de estructura para la ubicacion de los archivos referentes a los reportes.

--- SITEG/src/controlador/CReporteTematicasMasSolicitadas.java
+++ SITEG/src/controlador/CReporteTematicasMasSolicitadas.java
@@ -1,5 +1,8 @@
 package controlador;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -24,22 +27,30 @@ import modelo.ProgramaArea;
 import modelo.SolicitudTutoria;
 import modelo.Teg;
 import modelo.Tematica;
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 
 import org.springframework.stereotype.Controller;
+import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zkex.zul.Jasperreport;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Iframe;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
@@ -47,6 +58,7 @@ import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
 import configuracion.GeneradorBeans;
+import configuracion.UtilidadSiteg;
 
 import servicio.SAreaInvestigacion;
 import servicio.SPrograma;
@@ -67,6 +79,7 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 
 	Programa programa = new Programa();
 	AreaInvestigacion area = new AreaInvestigacion();
+
 	/********** Proyectos Solicitados **************/
 
 	@Wire
@@ -80,7 +93,9 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 	@Wire
 	private Combobox cmbEstatus;
 	@Wire
-	private Combobox cmbTipo;
+	private Combobox cmbEtapaTeg;
+	@Wire
+	private Jasperreport jstVistaPrevia;
 
 	@Override
 	void inicializar(Component comp) {
@@ -93,12 +108,13 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 
 	}
 
-	@Listen("onClick = #btnGenerarProyectosSolicitados")
+	
+	@Listen("onClick = #btnGenerarTematicasSolicitadas")
 	public void generarProyectosSolicitados() throws JRException {
 
 		String nombreArea = cmbArea.getValue();
 		String nombrePrograma = cmbPrograma.getValue();
-		String tipo = cmbTipo.getValue();
+		String etapaTeg = cmbEtapaTeg.getValue();
 		String estatus = cmbEstatus.getValue();
 		System.out.println("es" + estatus);
 		Date fechaInicio = new Date();
@@ -112,31 +128,37 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 					"La fecha de inicio debe ser primero que la fecha de fin",
 					"Error", Messagebox.OK, Messagebox.ERROR);
 		} else {
-			if (nombrePrograma != "Todos" && nombreArea != "Todos") {
+			if (!nombrePrograma.equals("Todos") && nombreArea != "Todos") {
 				String idArea = cmbArea.getSelectedItem().getId();
-				AreaInvestigacion area1 = servicioArea
-						.buscarArea(Long.parseLong(idArea));
+				AreaInvestigacion area1 = servicioArea.buscarArea(Long
+						.parseLong(idArea));
 				tematicasSeleccionadas = servicioTeg
 						.buscarUltimasTematicasProgramaAreaEstatus(estatus,
 								area1, fechaInicio, fechaFin);
 			}
-			else
+
 			if (nombrePrograma.equals("Todos")) {
 				tematicasSeleccionadas = servicioTeg.buscarUltimasEstatus(
 						estatus, fechaInicio, fechaFin);
 			}
-			else
-			if(nombrePrograma!="Todos"  && nombreArea.equals("Todos")) {
-				String idPrograma=cmbPrograma.getSelectedItem().getId();
-				Programa programa1=servicioPrograma.buscar(Long.parseLong(idPrograma));
-				System.out.println("programa"+programa1.getNombre());
+
+			if (!nombrePrograma.equals("Todos") && nombreArea.equals("Todos")) {
+				String idPrograma = cmbPrograma.getSelectedItem().getId();
+				Programa programa1 = servicioPrograma.buscar(Long
+						.parseLong(idPrograma));
+				System.out.println("programa" + programa1.getNombre());
 				tematicasSeleccionadas = servicioTeg
 						.buscarUltimasTematicasProgramaEstatus(estatus,
 								programa1, fechaInicio, fechaFin);
-			
+
 			}
-			
+
 		}
+		
+		
+		if(tematicasSeleccionadas.size()!=0){
+		
+		
 		/*************************** Contador de Lista ************* ******************/
 		List<Long> tematicas = new ArrayList();
 		List<Integer> contadores = new ArrayList();
@@ -177,21 +199,18 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 		}
 		if (contadoresOrdenados.size() < 5) {
 			int cantidadFaltante = 4 - contadoresOrdenados.size();
-			System.out.println("tamaño lista1:" + contadoresOrdenados.size());
 			int i;
 			for (i = 0; i < 4 - contadoresOrdenados.size(); i++) {
 				for (int z = 0; z < contadores.size(); z++) {
-					System.out.println("itemsz-:" + contadores.get(z));
 					contadoresOrdenados.add(contadores.get(z));
 					tematicasOrdenados.add(tematicas.get(z));
 				}
 			}
-			System.out.println("tamaño lista2:" + contadoresOrdenados.size());
 		}
 		if (contadoresOrdenados.size() > 5) {
-			for (int i = 0; i < 4; i++) {
-				contadoresOrdenados.add(contadores.get(i));
-				tematicasOrdenados.add(tematicas.get(i));
+			for (int i = 5; i < contadoresOrdenados.size(); i++) {
+				contadoresOrdenados.remove(i);
+				tematicasOrdenados.remove(i);
 			}
 		}
 		List<Long> tematicasFinales2 = new ArrayList();
@@ -199,29 +218,40 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 			Tematica tematicaFinal = servicioTematica
 					.buscarTematica(tematicasOrdenados.get(i));
 			tematicasFinales.add(tematicaFinal);
-			System.out.println("items3tematicas-:"
-					+ tematicasFinales.get(i).getId());
 		}
 		List<Teg> tegsSegunUltimasTematicas = servicioTeg
 				.buscarUltimasOrdenadasEstatus(estatus, tematicasFinales,
 						fechaInicio, fechaFin);
 
 		FileSystemView filesys = FileSystemView.getFileSystemView();
-		Map p = new HashMap();
-		p.put("titulo", "UNIVERSIDAD CENTROCCIDENTA LISANDRO ALVARADO");
-		p.put("programaNombre", cmbPrograma.getValue());
-		p.put("areaNombre", cmbArea.getValue());
-		JasperReport jasperReport = (JasperReport) JRLoader
-				.loadObject(getClass().getResource(
-						"/reporte/RProyectosSolicitados.jasper"));
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, p,
-				new JRBeanCollectionDataSource(tegsSegunUltimasTematicas));
-		JasperExportManager.exportReportToPdfFile(jasperPrint, filesys
-				.getHomeDirectory().toString()
-				+ "/ReporteTematicasMasSolicitadas.pdf");
-		Messagebox.show("Se ha generado exitosamente",
-				"Informacion", Messagebox.OK, Messagebox.INFORMATION);
+		Map parametro = new HashMap();
 
+		String rutaUrl = obtenerDirectorio();
+		String reporteSrc = rutaUrl
+				+ "SITEG/vistas/reportes/estadisticos/jasper/RTematicasMasSolicitadas.jasper";
+		String reporteImage=rutaUrl
+				+ "SITEG/public/imagenes/reportes/";
+		
+
+		parametro.put("titulo", "UNIVERSIDAD CENTROCCIDENTAL LISANDRO ALVARADO"
+				+ " DECANATO DE CIENCIAS Y TECNOLOGIA"
+				+ "DIRECCION DE PROGRAMA");
+		parametro.put("programaNombre", cmbPrograma.getValue());
+		parametro.put("areaNombre", cmbArea.getValue());
+		parametro.put("etapaTeg", etapaTeg);
+		parametro.put("logoUcla", reporteImage+"logo ucla.png");
+		parametro.put("logoCE", reporteImage+"logo CE.png");
+		
+		
+		jstVistaPrevia.setSrc(reporteSrc);
+		jstVistaPrevia.setDatasource(new JRBeanCollectionDataSource(
+				tegsSegunUltimasTematicas));
+		jstVistaPrevia.setType("pdf");
+		jstVistaPrevia.setParameters(parametro);
+		}
+		else{
+			Messagebox.show("No ha informacion disponible para este intervalo");
+		}
 
 	}
 
@@ -244,20 +274,36 @@ public class CReporteTematicasMasSolicitadas extends CGeneral {
 		}
 	}
 
-	@Listen("onSelect = #cmbTipo")
-	public void seleccionarTipo() throws JRException {
+	@Listen("onSelect = #cmbEtapaTeg")
+	public void seleccionarEtapaTeg() throws JRException {
 		List<String> listaComboEstatus = new ArrayList();
-		String tipoNombre = cmbTipo.getValue();
-		if (tipoNombre.equals("Proyecto")) {
-			// listaComboEstatus.add("Todos");
+		String etapaNombre = cmbEtapaTeg.getValue();
+		cmbEstatus.setDisabled(false);
+		if (etapaNombre.equals("Proyecto")) {
 			listaComboEstatus.add("Factible");
 			listaComboEstatus.add("No Factible");
+			
 		}
-		if (tipoNombre.equals("Teg")) {
-			// listaComboEstatus.add("Todos");
+		if (etapaNombre.equals("Teg")) {
 			listaComboEstatus.add("Aprobado");
 			listaComboEstatus.add("Reprobado");
 		}
 		cmbEstatus.setModel(new ListModelList<String>(listaComboEstatus));
 	}
+	@Listen("onClick = #btnCancelarTematicasSolicitadas")
+	public void cancelarTematicasSolicitadas() throws JRException {
+		
+		cmbPrograma.setValue("");
+		cmbArea.setValue("");
+		cmbArea.setDisabled(true);
+		cmbEtapaTeg.setValue("");
+		cmbEstatus.setValue("");
+		cmbEstatus.setDisabled(true);
+		dtbFechaInicio.setValue(new Date());
+		dtbFechaFin.setValue(new Date());
+		
+		jstVistaPrevia.setSrc("");
+		jstVistaPrevia.setDatasource(null);
 	}
+
+}
