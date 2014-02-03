0e3a1d27497e48a82e253de3836bb54c7f800658
mariuzcae.9@gmail.com <mariuzcae.9@gmail.com@9b7b6b13-42b4-688b-2d9a-da01cea6701c>
2014-02-03 01:22:59 +0000

    Reporte Defensa controlador

--- trunk/SITEG/src/controlador/CReporteDefensa.java
+++ trunk/SITEG/src/controlador/CReporteDefensa.java
@@ -1,5 +1,8 @@
+
 package controlador;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -8,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.mail.Address;
 import javax.mail.Message;
@@ -19,8 +23,8 @@ import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileSystemView;
 
 import modelo.AreaInvestigacion;
+import modelo.Defensa;
 import modelo.Estudiante;
-import modelo.Jurado;
 import modelo.Profesor;
 import modelo.Programa;
 import modelo.ProgramaArea;
@@ -34,6 +38,7 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 import org.springframework.stereotype.Controller;
 import org.zkoss.zk.ui.Component;
@@ -60,130 +65,131 @@ import servicio.SProgramaArea;
 import servicio.SSolicitudTutoria;
 import servicio.STeg;
 import servicio.STematica;
-
+import servicio.SDefensa;
+import modelo.Teg;
 @Controller
 public class CReporteDefensa extends CGeneral {
-	public static class ElementoReporte {
-		private String nombre;
-		private String titulo;
-		private String cargo;
-		private String estatusTeg;
-
-
-		public ElementoReporte(String nombre, String titulo, String cargo, String estatusTeg) {
-			super();
-			this.nombre = nombre;
-			this.titulo = titulo;
-			this.cargo = cargo;
-			this.estatusTeg = estatusTeg;
-		}
-
-		public String getNombre() {
-			return nombre;
-		}
-		
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-		
-		public String getTitulo() {
-			return titulo;
-		}
-		
-		public void setTitulo(String titulo) {
-			this.titulo = titulo;
-		}
-		
-		public String getCargo() {
-			return cargo;
-		}
-		
-		public void setCargo(String cargo) {
-			this.cargo = cargo;
-		}
-		
-		public String getEstatusTeg() {
-			return estatusTeg;
-		}
-
-		public void setEstatusTeg(String estatusTeg) {
-			this.estatusTeg = estatusTeg;
-		}
-	}
 	
-	SJurado servicioJurado = GeneradorBeans.getServicioJurado();
-	STeg servicioTeg = GeneradorBeans.getServicioTeg();
+	public CReporteDefensa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static class ElementoReporte {
+		
+		private String tituloTeg;
+		private String nombreTutor;
+		private String nombreEstudiante;
+		private String estatusDefensa;
+		private Date fechaDefensa ;
+
+
+	 public ElementoReporte(String tituloteg, String nombretutor, String nombreEstudiante, String estatusdefensa, Date fechaDefensa) {
+			super();
+			this.tituloTeg = tituloteg;
+			this.nombreTutor = nombretutor;
+			this.nombreEstudiante = nombreEstudiante;
+			this.estatusDefensa = estatusdefensa;
+			this.fechaDefensa= fechaDefensa;
+		}
+
+
+	public String getTituloTeg() {
+		return tituloTeg;
+	}
+
+
+	public void setTituloTeg(String tituloTeg) {
+		this.tituloTeg = tituloTeg;
+	}
+
+
+	public String getNombreTutor() {
+		return nombreTutor;
+	}
+
+
+	public void setNombreTutor(String nombreTutor) {
+		this.nombreTutor = nombreTutor;
+	}
+
+
+	public String getEstatusDefensa() {
+		return estatusDefensa;
+	}
+
+
+	public void setEstatusDefensa(String estatusDefensa) {
+		this.estatusDefensa = estatusDefensa;
+	}
+
+
+	public String getNombreEstudiante() {
+		return nombreEstudiante;
+	}
+
+
+	public void setNombrestudiante(String nombrestudiante) {
+		this.nombreEstudiante = nombrestudiante;
+	}
+
+
+	public Date getFechaDefensa() {
+		return fechaDefensa;
+	}
+
+
+	public void setFechaDefensa(Date fechaDefensa) {
+		this.fechaDefensa = fechaDefensa;
+	}
+
+	 
+	 
+	}
+
 	SPrograma servicioPrograma = GeneradorBeans.getServicioPrograma();
 	SAreaInvestigacion servicioArea = GeneradorBeans.getServicioArea();
-	STematica servicioTematica = GeneradorBeans.getSTematica();
-	SProgramaArea servicioProgramaArea = GeneradorBeans
-			.getServicioProgramaArea();
-	CCatalogoProfesorTematica catalogo = new CCatalogoProfesorTematica();
-	CCatalogoProfesor catalogoProfesor = new CCatalogoProfesor();
+	SDefensa servicioDefensa = GeneradorBeans.getServicioDefensa();
+	
 	@Wire
-	private Window wdwReporteProfesorCargo;
+	private Window wdwReporteDefensa;
+	
 	@Wire
-	private Datebox dtbFechaReporteProfesorTeg;
-	@Wire
-	private Datebox dtbCronogramaFechaInicio;
+	private Datebox dtbFechaInicio;
 
 	@Wire
-	private Datebox dtbCronogramaFechaHasta;
-	@Wire
-	private Textbox txtCedulaReporteProfesorTeg;
-	@Wire
-	private Combobox cmbEstatus;
+	private Datebox dtbFechaFin;
+
 	@Wire
 	private Combobox cmbPrograma;
+	
 	@Wire
 	private Combobox cmbArea;
 	@Wire
 	private Combobox cmbTematica;
-	private String[] estatusProfesor = { "Todos", "Tutor",
-			"Comision Evaluadora", "Jurado" };
+	@Wire
+	private Combobox cmbEstatus;
+	
+	
+	
+	private String[] estatusDefensa = { "Todos", "Por Defender", "Defensa Finalizada"};
 	List<AreaInvestigacion> areas = new ArrayList<AreaInvestigacion>();
 	List<Tematica> tematicas = new ArrayList<Tematica>();
 	List<Programa> programas = new ArrayList<Programa>();
 	long idTematica = 0;
 	long idArea = 0;
-
 	@Override
+	
 	void inicializar(Component comp) {
-		// TODO Auto-generated method stub
-		programas = servicioPrograma.buscarActivas();
-		Programa programaa = new Programa(987, "Todos", "", "", true, null);
-		programas.add(programaa);
-
+		cmbEstatus.setModel(new ListModelList<String>(estatusDefensa));
+		List<Programa> programas = servicioPrograma.buscarActivas();
 		cmbPrograma.setModel(new ListModelList<Programa>(programas));
-
-		cmbPrograma.setModel(new ListModelList<Programa>(programas));
-		cmbEstatus.setModel(new ListModelList<String>(estatusProfesor));
-
-		Selectors.wireComponents(comp, this, false);
-		HashMap<String, Object> map = (HashMap<String, Object>) Sessions
-				.getCurrent().getAttribute("itemsCatalogo");
-
-		if (map != null) {
-			if ((String) map.get("cedula") != null) {
-				Profesor profesor = servicioProfesor
-						.buscarProfesorPorCedula((String) map.get("cedula"));
-				txtCedulaReporteProfesorTeg.setValue(profesor.getCedula());
-
-				idTematica = (Long) map.get("tematica");
-
-				Programa programa = servicioPrograma.buscar((Long) map
-						.get("programa"));
-				Tematica tematica = servicioTematica.buscarTematica((Long) map
-						.get("tematica"));
-				cmbPrograma.setValue(programa.getNombre());
-				cmbArea.setValue((String) map.get("area"));
-				cmbTematica.setValue(tematica.getNombre());
-			}
-		}
+		
 	}
+	
 
 	@Listen("onSelect = #cmbPrograma")
-	public void seleccinarPrograma() {
+	public void seleccionarPrograma() {
 		cmbArea.setValue("");
 		cmbTematica.setValue("");
 
@@ -208,37 +214,21 @@ public class CReporteDefensa extends CGeneral {
 		Tematica tematica = (Tematica) cmbTematica.getSelectedItem().getValue();
 		idTematica = tematica.getId();
 	}
-
-	@Listen("onClick = #btnProfesorReporteProfesorTeg")
-	public void buscarProfesor() {
-
-		final HashMap<String, Object> map2 = new HashMap<String, Object>();
-
-		map2.put("area", cmbArea.getValue());
-		Sessions.getCurrent().setAttribute("itemsCatalogo", map2);
-		catalogo.recibir("reportes/no estructurados/VReporteProfesorTeg",
-				Long.parseLong(cmbPrograma.getSelectedItem().getId()),
-				Long.parseLong(cmbTematica.getSelectedItem().getId()));
-
-		Window window = (Window) Executions.createComponents(
-				"/vistas/catalogos/VCatalogoProfesorTematica.zul", null, null);
-		window.doModal();
-
-	}
-
-	@Listen("onClick = #btnGenerarReporteProfesorCargo")
-	public void generarReporteProfesorTeg() throws JRException {
-		Date fechaInicio = dtbCronogramaFechaInicio.getValue();
-		Date fechaFin = dtbCronogramaFechaHasta.getValue();
+	
+	@Listen("onClick = #btnGenerarReporteDefensa")
+	public void generarReporteDefensa() throws JRException {
+		Date fechaInicio = dtbFechaInicio.getValue();
+		Date fechaFin = dtbFechaFin.getValue();
 		String area1 = cmbArea.getValue();
 		String tematica1 = cmbTematica.getValue();
 		String programa1= cmbPrograma.getValue();
+		String estatusdefensa =cmbEstatus.getValue();
 		AreaInvestigacion area = servicioArea.buscarArea(idArea);
 		Tematica tematica = servicioTematica.buscarTematica(idTematica);
-		String tipoCargo = (String) cmbEstatus.getSelectedItem().getValue();
-		System.out.println(tipoCargo);
-		//Profesor profesor = servicioProfesor.buscarProfesorPorCedula(cedula);
-
+		String tipoDefensa = (String) cmbEstatus.getSelectedItem().getValue();
+		System.out.println(tipoDefensa);
+		
+		
 		List<ElementoReporte> elementos = new ArrayList<ElementoReporte>();
 
 		if (fechaFin == null || fechaInicio == null || fechaInicio.after(fechaFin)) {
@@ -246,56 +236,54 @@ public class CReporteDefensa extends CGeneral {
 					"La fecha de inicio debe ser primero que la fecha de fin",
 					"Error", Messagebox.OK, Messagebox.ERROR);
 		} else {
-			List<Teg> tegs = servicioTeg.buscarTegsDeTematicaPorDosFechas(tematica, fechaInicio, fechaFin);
-			for (Teg teg : tegs) {
-				Profesor profesorTutor = teg.getTutor();
-				if (tipoCargo.equals("Tutor") || tipoCargo.equals("Todos")) {
+			List<Defensa> defensas = servicioDefensa.buscardefensaPorDosFechas(fechaInicio, fechaFin);
+			for (Defensa defensa : defensas) {
+				 
+					 List<Estudiante> estudiantes= servicioEstudiante.buscarEstudiantePorTeg(defensa.getTeg());
+					 
+							String nombreEstudiantes = "";
+							for (Estudiante e : estudiantes) {
+							nombreEstudiantes += e.getNombre() +" "+e.getApellido()+" ";
+							}
+						
+				if (defensa.getEstatus().equals("Por Defender") || defensa.getEstatus().equals("Todos")) {
 					elementos.add(new ElementoReporte(
-							profesorTutor.getNombre() + " " + profesorTutor.getApellido(),
-							teg.getTitulo(),
-							"Tutor", teg.getEstatus()));
+							defensa.getTeg().getTitulo(),
+							defensa.getProfesor().getNombre() + " " + defensa.getProfesor().getApellido(),
+							nombreEstudiantes,
+							defensa.getEstatus(),
+							defensa.getFecha()));
 				}
-				
-				if (tipoCargo.equals("Jurado") || tipoCargo.equals("Todos")) {
-					for (Jurado jurado : servicioJurado.buscarJuradoDeTeg(teg)) {
-						Profesor profesorJurado = jurado.getProfesor();
-						elementos.add(new ElementoReporte(
-								profesorJurado.getNombre() + " " + profesorJurado.getApellido(),
-								teg.getTitulo(),
-								"Jurado - " + jurado.getTipoJurado().getNombre(), teg.getEstatus()));
-					}
-				}
-				
-				if (tipoCargo.equals("Comision Evaluadora") || tipoCargo.equals("Todos")) {
-					for (Profesor profesorComision : servicioProfesor.buscarComisionDelTeg(teg)) {
-						elementos.add(new ElementoReporte(
-								profesorComision.getNombre() + " " + profesorComision.getApellido(),
-								teg.getTitulo(),
-								"Comision", teg.getEstatus()));
-					}
+				else 
+				if (defensa.getEstatus().equals("Defensa Finalizada") || defensa.getEstatus().equals("Todos")) {
+							elementos.add(new ElementoReporte(
+							defensa.getTeg().getTitulo(),
+							defensa.getProfesor().getNombre() + " " + defensa.getProfesor().getApellido(),
+							nombreEstudiantes,
+							defensa.getEstatus(),
+							defensa.getFecha()));
 				}
 			}
-
+			
 			Collections.sort(elementos, new Comparator<ElementoReporte>() {
 				public int compare(ElementoReporte a, ElementoReporte b) {
-				return a.nombre.compareTo(b.nombre);
+				return a.tituloTeg.compareTo(b.tituloTeg);
 				}});
 		}
 
 		FileSystemView filesys = FileSystemView.getFileSystemView();
 		Map<String, Object> p = new HashMap<String, Object>();
-		p.put("titulo", "LISTA DE PROFESORES CON EL ROL QUE DESEMPEÑAN EN CADA TRABAJO ESPECIAL DE GRADO");
 		p.put("fecha", new Date());
 		p.put("fecha1", fechaInicio);
 		p.put("fecha2", fechaFin);
 		p.put("area", area1);
 		p.put("tematica", tematica1);
 		p.put("programa", programa1);
-		p.put("cargo", tipoCargo);
+		p.put("estatus", estatusdefensa);
 
 		JasperReport jasperReport = (JasperReport) JRLoader
 				.loadObject(getClass().getResource(
-						"/reporte/RReporteProfesorCargo.jasper"));
+						"/reporte/RReporteDefensa.jasper"));
 
 		JasperPrint jasperPrint = JasperFillManager.fillReport(
 				jasperReport, p, new JRBeanCollectionDataSource(elementos));
@@ -308,13 +296,16 @@ public class CReporteDefensa extends CGeneral {
 				Messagebox.INFORMATION);
 
 	}
-	@Listen("onClick = #btnSalirReporteProfesorCargo")
-	public void cancelarItem() {
-		cmbEstatus.setValue("");
+
+	@Listen("onClick = #btnSalirReporteSolicitud")
+	public void cancelarTematicasSolicitadas() throws JRException {
+
 		cmbPrograma.setValue("");
 		cmbArea.setValue("");
-		cmbTematica.setValue("");;
-		
+		cmbTematica.setValue("");
+		dtbFechaInicio.setValue(new Date());
+		dtbFechaFin.setValue(new Date());	
 	}
+	
 }
  
