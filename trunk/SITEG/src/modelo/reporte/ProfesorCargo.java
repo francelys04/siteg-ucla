0e566d13d45b600a8ea2fcfda5812ee1a0248dc1
irinap.209@gmail.com <irinap.209@gmail.com@9b7b6b13-42b4-688b-2d9a-da01cea6701c>
2014-02-04 05:10:36 +0000

    rep viejoooooo

---
+++ trunk/SITEG/src/modelo/reporte/ProfesorCargo.java
@@ -0,0 +1,49 @@
+package modelo.reporte;
+
+public class ProfesorCargo {
+		private String nombre;
+		private String titulo;
+		private String cargo;
+		private String estatusTeg;
+
+
+		public ProfesorCargo(String nombre, String titulo, String cargo, String estatusTeg) {
+			super();
+			this.nombre = nombre;
+			this.titulo = titulo;
+			this.cargo = cargo;
+			this.estatusTeg = estatusTeg;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+		
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		
+		public String getTitulo() {
+			return titulo;
+		}
+		
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+		
+		public String getCargo() {
+			return cargo;
+		}
+		
+		public void setCargo(String cargo) {
+			this.cargo = cargo;
+		}
+		
+		public String getEstatusTeg() {
+			return estatusTeg;
+		}
+
+		public void setEstatusTeg(String estatusTeg) {
+			this.estatusTeg = estatusTeg;
+		}
+	}
