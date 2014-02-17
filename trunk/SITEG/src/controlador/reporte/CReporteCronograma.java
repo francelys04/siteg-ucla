d04f33554fe68483a41974663a3a4b9a6ad70e92
davidrodriguez100415@gmail.com <davidrodriguez100415@gmail.com@9b7b6b13-42b4-688b-2d9a-da01cea6701c>
2014-02-17 01:05:26 +0000

    Aplicacion de comentarios a controladores de reportes

--- trunk/SITEG/src/controlador/reporte/CReporteCronograma.java
+++ trunk/SITEG/src/controlador/reporte/CReporteCronograma.java
@@ -1,4 +1,5 @@
 package controlador.reporte;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -43,83 +44,86 @@ import servicio.SLapso;
 import servicio.SPrograma;
 import configuracion.GeneradorBeans;
 import controlador.CGeneral;
+
 @Controller
 public class CReporteCronograma extends CGeneral {
-	SPrograma servicioPrograma = GeneradorBeans.getServicioPrograma();
-	SLapso servicioLapso = GeneradorBeans.getServicioLapso();
-	SCronograma servicioCronograma = GeneradorBeans.getServicioCronograma();
-	
+
 	@Wire
 	private Combobox cmbCronogramaPrograma;
 	@Wire
-	private Combobox  cmbCronogramaLapso;
+	private Combobox cmbCronogramaLapso;
 	@Wire
 	private Window wdwReporteCronograma;
 
-
+	/*
+	 * Metodo heredado del Controlador CGeneral donde se buscan todas los
+	 * programas y lapsos disponibles y se llena el listado del mismo en el
+	 * componente lista de la vista.
+	 */
 	@Override
-	public
-	void inicializar(Component comp) {
+	public void inicializar(Component comp) {
 		// TODO Auto-generated method stub
 		List<Programa> programas = servicioPrograma.buscarActivas();
 		List<Lapso> lapsos = servicioLapso.buscarActivos();
-		
+
 		cmbCronogramaLapso.setModel(new ListModelList<Lapso>(lapsos));
-		cmbCronogramaPrograma.setModel(new ListModelList<Programa>(
-				programas));
-		
+		cmbCronogramaPrograma.setModel(new ListModelList<Programa>(programas));
+
 	}
 
+	/*
+	 * Metodo que permite generar un reporte, dado un programa y un lapso se
+	 * generara un pdf donde se muestra una lista de las actividades de esta
+	 * seleccion, mediante el componente "Jasperreport" donde se mapea una serie
+	 * de parametros y una lista previamente cargada que seran los datos que se
+	 * muestra en el documento.
+	 */
 	@Listen("onClick = #btnGenerarReporteCronograma")
-	 public void GenerarCronograma() throws JRException, IOException{
-		if ((cmbCronogramaPrograma.getValue() =="") || (cmbCronogramaLapso.getValue()=="")) {
-			    Messagebox.show(
-					  "Datos imcompletos",
-					     "Informacion", Messagebox.OK,
-					   Messagebox.INFORMATION);
-		   }
-		else{
-		     Lapso lapso = servicioLapso.buscarLapso(Long
-				.parseLong(cmbCronogramaLapso.getSelectedItem().getId()));
-		     Programa programa = servicioPrograma
-				.buscar((Long.parseLong(cmbCronogramaPrograma
-						.getSelectedItem().getId())));
-		     FileSystemView filesys = FileSystemView.getFileSystemView();
-		     List<Cronograma> cronograma = servicioCronograma.buscarCronogramaPorLapsoYPrograma(programa, lapso);
-		     String rutaUrl = obtenerDirectorio();
-		     String reporteSrc = rutaUrl
-					 +
-					 "SITEG/vistas/reportes/salidas/compilados/report1.jasper";
-		     String reporteImage = rutaUrl + "SITEG/public/imagenes/reportes/";
-		   
-		    
-		     Map p = new HashMap();
-		
-		     p.put("programa", cmbCronogramaPrograma.getValue());
-		     System.out.println(cmbCronogramaPrograma.getValue());
-		     p.put("lapso", cmbCronogramaLapso.getValue());	
-		     p.put("logoUcla", reporteImage + "logo ucla.png");
-		 	 p.put("logoCE", reporteImage + "logo CE.png");
-		     p.put("logoSiteg", reporteImage + "logo.png");
-		     
+	public void GenerarCronograma() throws JRException, IOException {
+		if ((cmbCronogramaPrograma.getValue() == "")
+				|| (cmbCronogramaLapso.getValue() == "")) {
+			Messagebox.show("Datos imcompletos", "Informacion", Messagebox.OK,
+					Messagebox.INFORMATION);
+		} else {
+			Lapso lapso = servicioLapso.buscarLapso(Long
+					.parseLong(cmbCronogramaLapso.getSelectedItem().getId()));
+			Programa programa = servicioPrograma
+					.buscar((Long.parseLong(cmbCronogramaPrograma
+							.getSelectedItem().getId())));
+			FileSystemView filesys = FileSystemView.getFileSystemView();
+			List<Cronograma> cronograma = servicioCronograma
+					.buscarCronogramaPorLapsoYPrograma(programa, lapso);
+			String rutaUrl = obtenerDirectorio();
+			String reporteSrc = rutaUrl
+					+ "SITEG/vistas/reportes/salidas/compilados/report1.jasper";
+			String reporteImage = rutaUrl + "SITEG/public/imagenes/reportes/";
 
-			JasperReport jasperReport = (JasperReport) JRLoader.loadObject(reporteSrc);
-		     JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, p, new JRBeanCollectionDataSource(cronograma));
-		     JasperExportManager.exportReportToPdfFile(jasperPrint, filesys.getHomeDirectory().toString()+"/ReporteCronograma.pdf"); 
-		     Messagebox.show(
-				"Se ha generado exitosamente el reporte",
-				"Informacion", Messagebox.OK,
-				Messagebox.INFORMATION);
+			Map p = new HashMap();
+
+			p.put("programa", cmbCronogramaPrograma.getValue());
+			System.out.println(cmbCronogramaPrograma.getValue());
+			p.put("lapso", cmbCronogramaLapso.getValue());
+			p.put("logoUcla", reporteImage + "logo ucla.png");
+			p.put("logoCE", reporteImage + "logo CE.png");
+			p.put("logoSiteg", reporteImage + "logo.png");
+
+			JasperReport jasperReport = (JasperReport) JRLoader
+					.loadObject(reporteSrc);
+			JasperPrint jasperPrint = JasperFillManager
+					.fillReport(jasperReport, p,
+							new JRBeanCollectionDataSource(cronograma));
+			JasperExportManager.exportReportToPdfFile(jasperPrint, filesys
+					.getHomeDirectory().toString() + "/ReporteCronograma.pdf");
+			Messagebox.show("Se ha generado exitosamente el reporte",
+					"Informacion", Messagebox.OK, Messagebox.INFORMATION);
 		}
-		
+
 	}
+	/*Metodo que permite cerrar la vista*/
 	@Listen("onClick = #btnCancelarReporteCronograma")
-   public void Salir(){
-	   wdwReporteCronograma.onClose();
-   
-   }
-	
-	
+	public void Salir() {
+		wdwReporteCronograma.onClose();
+
 	}
 
-
+}
